f878066eef19ba56b71aba8c4e449f69213f9559
Phat Nguyen <beeiscoding@gmail.com>
2018-04-20 02:31:59 +0700

    [Func] Inject dependency in Application class

---
+++ app/src/main/java/twitsplit/project/bee/twitsplit/App.java
@@ -0,0 +1,39 @@
+package twitsplit.project.bee.twitsplit;
+
+import android.app.Application;
+import android.content.Context;
+
+import twitsplit.project.bee.twitsplit.di.di.component.ApplicationComponent;
+import twitsplit.project.bee.twitsplit.di.di.component.DaggerApplicationComponent;
+import twitsplit.project.bee.twitsplit.di.di.module.ApplicationModule;
+
+public class App extends Application {
+    private ApplicationComponent appComponent;
+
+    public static ApplicationComponent getApplicationComponent(Context context) {
+        return ((App) context
+                .getApplicationContext())
+                .getAppComponent();
+    }
+
+    public ApplicationComponent getAppComponent() {
+        return appComponent;
+    }
+
+    public void setAppComponent(ApplicationComponent appComponent) {
+        this.appComponent = appComponent;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        buildAppComponent();
+    }
+
+    private void buildAppComponent() {
+        appComponent = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+        appComponent.inject(this);
+    }
+}
